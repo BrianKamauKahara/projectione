7680aab2fc1c2899c10314205c8447fbc69b808f
BrianKamauKahara <167960457+BrianKamauKahara@users.noreply.github.com>
2024-06-07 07:49:28 +0300

    Updated files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup import find_packages, setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='brian kamau',
+    author_email='theemeraldprogamer@gmail.com',
+    install_requires=['openai','langchain','streamlit','python-dotenv','PyPDF2'],
+    packages=find_packages()
+)
