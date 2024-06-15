a26b0bdb81243dba10a691a84b48a80c48eb11b8
BrianKamauKahara <167960457+BrianKamauKahara@users.noreply.github.com>
2024-06-15 09:41:50 +0300

    debugging

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -37,49 +37,50 @@ with st.form("user_inputs"):
 
     if button and uploaded_file is not None and mcq_count and subject and tone:
         with st.spinner("loading..."):
-            try:
-                text = read_file(uploaded_file)
-                # Count tokens and the cost of API call
-                with get_openai_callback() as cb:
-                    #You need a working API
-                    response = generate_evaluate_chain(
-                        {
-                            "text": text,
-                            "number": mcq_count,
-                            "subject": subject,
-                            "tone": tone,
-                            "response_json": json.dumps(RESPONSE_JSON)
-                        }
-                        
-                    )
-                   
-                # st.write(response)
-
-            except Exception as e:
-                traceback.print_exception(type(e), e, e.__traceback__)
-                
-            ##st.error("Error")
-
-            finally:
-                print(f"Total Tokens:{cb.total_tokens}")
-                print(f"Prompt Tokens:{cb.prompt_tokens}")
-                print(f"Completion Tokens:{cb.completion_tokens}")
-                print(f"Total Cost:{cb.total_cost}")
-                if isinstance(response, dict):
-                    # Extract the quiz data from the response
-                    quiz = response.get("quiz", None)
-                    if quiz is not None:
-                        table_data = get_table_data(quiz)
-                        print(table_data ,'--------------------------')
-                        if table_data[0]:
-                            df = pd.DataFrame(table_data)
-                            df.index = df.index + 1
-                            st.table(df)
-                            # Display the review in a text box as well
-                            st.text_area(label="Review", value=response["review"])
-                        else:
-                            st.error("Could not generate table with that data")
+            with get_openai_callback() as cb:
+                try:
+                    text = read_file(uploaded_file)
+                    # Count tokens and the cost of API call
                     
-                    else:
-                        st.write(response)
+                        #You need a working API
+                        response = generate_evaluate_chain(
+                            {
+                                "text": text,
+                                "number": mcq_count,
+                                "subject": subject,
+                                "tone": tone,
+                                "response_json": json.dumps(RESPONSE_JSON)
+                            }
+                            
+                        )
+                    
+                    # st.write(response)
+
+                except Exception as e:
+                    traceback.print_exception(type(e), e, e.__traceback__)
+                    
+                ##st.error("Error")
+
+                finally:
+                    print(f"Total Tokens:{cb.total_tokens}")
+                    print(f"Prompt Tokens:{cb.prompt_tokens}")
+                    print(f"Completion Tokens:{cb.completion_tokens}")
+                    print(f"Total Cost:{cb.total_cost}")
+                    if isinstance(response, dict):
+                        # Extract the quiz data from the response
+                        quiz = response.get("quiz", None)
+                        if quiz is not None:
+                            table_data = get_table_data(quiz)
+                            print(table_data ,'--------------------------')
+                            if table_data[0]:
+                                df = pd.DataFrame(table_data)
+                                df.index = df.index + 1
+                                st.table(df)
+                                # Display the review in a text box as well
+                                st.text_area(label="Review", value=response["review"])
+                            else:
+                                st.error("Could not generate table with that data")
+                        
+                        else:
+                            st.write(response)
 
